2dfbf1a0413551291f0657bb2556be905066c056
Micxjo Funkcio <micxjo@fastmail.com>
2015-12-06 16:01:08 -0800

    Add day 5, part 2

--- advent/day5-test.rkt
+++ advent/day5-test.rkt
@@ -14,6 +14,13 @@
     (check-pred is-nice? "aaa")
     (check-false (is-nice? "jchzalrnumimnmhp"))
     (check-false (is-nice? "haegwjzuvuyypxyu"))
-    (check-false (is-nice? "dvszwmarrgswjxmb")))))
+    (check-false (is-nice? "dvszwmarrgswjxmb")))
+
+   (test-case
+    "Part 2: is-nice-2?"
+    (check-pred is-nice-2? "qjhvhtzxzqqjkmpb")
+    (check-pred is-nice-2? "xxyxx")
+    (check-false (is-nice? "uurcxstgmygtbstg"))
+    (check-false (is-nice? "ieodomkazucvgmuy")))))
 
 (run-tests tests)
--- advent/day5.rkt
+++ advent/day5.rkt
@@ -1,10 +1,12 @@
 #lang racket
 
-(provide is-nice?)
+(provide is-nice?
+         is-nice-2?)
 
 (require srfi/1
          srfi/13
-         racket/match)
+         racket/match
+         racket/cmdline)
 
 (define (is-vowel? c)
   (find (lambda (x) (eqv? x c))
@@ -19,6 +21,15 @@
       [_ acc]))
   (reverse (loop lst empty)))
 
+(define (trigrams lst)
+  (define (loop lst acc)
+    (match lst
+      [(cons a (cons b (cons c rest)))
+       (loop (cons b (cons c rest))
+             (cons (list a b c) acc))]
+      [_ acc]))
+  (reverse (loop lst empty)))
+
 (define (is-nice? str)
   (let* ([lst (string->list str)]
          [vowel-count (length (filter is-vowel? lst))]
@@ -30,3 +41,25 @@
     (and (>= vowel-count 3)
          has-good-bigram
          (not has-bad-string))))
+
+(define (is-good-trigram? lst)
+  (match lst
+    [(list a _ a) #t]
+    [_ #f]))
+
+(define (is-nice-2? str)
+  (let* ([lst (string->list str)]
+         [has-good-bigram (regexp-match #px"(..).*\\1" str)]
+         [has-good-trigram (ormap is-good-trigram?
+                                  (trigrams lst))])
+    (and has-good-bigram has-good-trigram)))
+
+(define filename (command-line
+                  #:args ([filename #f])
+                  filename))
+
+(when filename
+  (let* ([lines (file->lines filename)]
+         [part1 (length (filter is-nice? lines))]
+         [part2 (length (filter is-nice-2? lines))])
+    (printf "Part 1: ~a~nPart 2: ~a~n" part1 part2)))
